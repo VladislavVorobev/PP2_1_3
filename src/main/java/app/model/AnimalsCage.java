85e1d9d5a6c9982d9681454bb949277d01e651de
Zaliappara <124375115+VladislavVorobev@users.noreply.github.com>
2023-12-04 23:13:36 +0200

    Update AnimalsCage

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
+
+
+
 @Component
 public class AnimalsCage {
     @Autowired
@@ -13,15 +16,16 @@ public class AnimalsCage {
     @Autowired
     private Timer timer;
 
-    public Timer getTimer(){
-        return timer;
-    }
+
 
     public void whatAnimalSay() {
         System.out.println("Say:");
         System.out.println(animal.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
+        System.out.println(timer.getTime());
         System.out.println("________________________");
+    }
+    public Timer getTimer(){
+        return timer;
     }
 }
